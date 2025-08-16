45ab54571a3f0027be669e2e9d7fd3949c8ee027
BarbosaXBarbosa <asobrab123@hotmail.com>
2025-08-16 17:44:15 -0300

    add: Adicionando estrutura base do projeto.
    Refs: master

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [ApiController]
+    [Route("[api/projects]")]
+    public class ProjectsController : ControllerBase
+    {
+        
+    } 
+}    
---
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [ApiController]
+    [Route("[api/skills]")]
+    public class SkillsController : ControllerBase
+    {
+
+
+
+
+
+    } 
+}
