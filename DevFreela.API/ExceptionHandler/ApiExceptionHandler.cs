d97c662eb446ff90b22ce83f653c05dffe2a16f2
João Barbosa <joao.barbosa@progere.com.br>
2025-08-18 10:51:09 -0300

    add: Implementando Gerenciador de Exceções.
    Refs: master

---
+++ DevFreela.API/ExceptionHandler/ApiExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.ExceptionHandler
+{
+    public class ApiExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Title = "An error occurred while processing your request.",
+                Status = StatusCodes.Status500InternalServerError,
+                Detail = exception.Message,
+                Instance = context.Request.Path
+            };
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "application/problem+json";
+            await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            return true;
+        }
+    }
+}
--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -1,3 +1,4 @@
+using DevFreela.API.ExceptionHandler;
 using DevFreela.API.Models.Config;
 using DevFreela.API.Services;
 
@@ -13,6 +14,9 @@ builder.Services.Configure<PlatformConfig>(
 // registra o service que unifica configs
 builder.Services.AddScoped<IConfigService, ConfigService>();
 
+builder.Services.AddExceptionHandler<ApiExceptionHandler>();
+builder.Services.AddProblemDetails();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseExceptionHandler();
 
 app.UseHttpsRedirection();
 
