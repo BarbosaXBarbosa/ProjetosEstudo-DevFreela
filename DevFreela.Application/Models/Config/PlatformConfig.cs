c3139f21f222c91c98ae640936d1e7e316eadeb9
João Barbosa <joao.barbosa@progere.com.br>
2025-08-25 12:16:39 -0300

    feat(Infrastructure) Gerando e aplicando migrations com a nova estrutura.

--- DevFreela.Application/Models/Config/PlatformConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevFreela.API.Models.Config
-{
-    public class PlatformConfig
-    {
-        public decimal Commission { get; set; } // ex: 0.10 => 10%
-        public int ProposalExpirationDays { get; set; }
-    }
-}
